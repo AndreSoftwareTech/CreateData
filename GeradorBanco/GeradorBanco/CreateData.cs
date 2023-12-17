d1336737a59cb1e5a460f9f535de402d2fd98b89
Andre Granemann <softwaretech0210@gmail.com>
2023-12-17 10:36:09 -0300

    Subindo codigo para gerar um arquivo de banco de dados sqlite

---
+++ GeradorBanco/GeradorBanco/CreateData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorBanco;
+
+public class CreateData
+{
+    public void CriarBancoDeDados(string caminhoBancoDados)
+    {
+        using (SQLiteConnection conexao = new SQLiteConnection($"Data Source={caminhoBancoDados};Version=3;"))
+        {
+            conexao.Open();
+
+            // Comando SQL para criar uma tabela simples
+            string sqlCriarTabela = "CREATE TABLE IF NOT EXISTS Exemplo (ID INTEGER PRIMARY KEY AUTOINCREMENT, Nome TEXT NOT NULL, Idade INTEGER);";
+
+            using (SQLiteCommand comando = new SQLiteCommand(sqlCriarTabela, conexao))
+            {
+                comando.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ GeradorBanco/GeradorBanco/Program.cs
@@ -0,0 +1,15 @@
+﻿using GeradorBanco;
+
+
+
+CreateData createData = new CreateData();
+
+
+string caminhoBancoDados = "C:\\Users\\Andre\\OneDrive\\Documents\\repositorios\\Csharp\\WebProway\\BancoDeDados.sqlite";
+
+
+
+createData.CriarBancoDeDados(caminhoBancoDados);
+
+
+Console.WriteLine($"Banco de dados criado com sucesso em: {caminhoBancoDados}");
